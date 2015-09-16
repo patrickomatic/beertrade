beff84d1c778c3ff0c5315902ccedcc3b81c1bd8
Patrick Carroll <patrick@patrickomatic.com>
2015-09-16 12:34:01 -0400

    More consistent wording/capitalization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:id])
-    @pending = Trade.with_user(@user).pending.page(params[:pending_page])
+    @pending = Trade.with_user(@user).not_completed_yet.page(params[:pending_page])
     @completed = Trade.with_user(@user).completed.page(params[:completed_page])
     @notifications = Notification.for_user(@user).page(params[:notification_page])
   end
