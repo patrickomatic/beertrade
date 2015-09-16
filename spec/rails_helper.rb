cbd2e4461539fdee18146b686cf8b93d37da13d7
Patrick Carroll <patrick@patrickomatic.com>
2015-09-16 12:34:15 -0400

    log_in_as helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,9 +27,6 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -49,4 +46,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+end
+
+def log_in_as(user)
+  allow(controller).to receive(:current_user).and_return(user)
 end
