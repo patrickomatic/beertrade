4eca388b603085dddbe5d995c1339b14fd97afde
Patrick Carroll <patrick@patrickomatic.com>
2015-10-07 20:24:05 -0400

    Notification styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:id])
+
     @pending = @user.trades.not_completed_yet.page(params[:pending_page])
-    @completed = @user.trades.completed.page(params[:completed_page])
+
+    @completed = if @feedback_filter = feedback_filter_param
+                   @user.trades.completed.with_feedback(@feedback_filter).page(params[:completed_page])
+                 else
+                   @user.trades.completed.page(params[:completed_page])
+                 end
 
     @notifications = if current_user == @user
                        @user.notifications.page(params[:notification_page])
@@ -15,4 +21,15 @@ class UsersController < ApplicationController
   def index
     @users = User.by_feedback.page(params[:page])
   end
+
+
+  private
+
+    def feedback_filter_param
+      if %w(positive neutral negative).include? params[:feedback].to_s
+        params[:feedback].to_sym
+      else
+        nil
+      end
+    end
 end
