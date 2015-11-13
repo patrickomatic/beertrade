caec1f1aa292749fc8d654215b6de0258493496a
Patrick Carroll <patrick@patrickomatic.com>
2015-11-12 19:17:35 -0500

    This syntax was wrong

--- db/migrate/20151024204508_add_lowercase_username_index_to_users.rb
+++ db/migrate/20151024204508_add_lowercase_username_index_to_users.rb
@@ -1,11 +1,9 @@
 class AddLowercaseUsernameIndexToUsers < ActiveRecord::Migration
-  def change
-    def up
-      execute "CREATE UNIQUE INDEX index_users_on_lower_username ON users (lower(username))"
-    end
+  def up
+    execute "CREATE UNIQUE INDEX index_users_on_lower_username ON users (lower(username))"
+  end
 
-    def down
-      execute "DROP INDEX index_users_on_lower_username"
-    end
+  def down
+    execute "DROP INDEX index_users_on_lower_username"
   end
 end
