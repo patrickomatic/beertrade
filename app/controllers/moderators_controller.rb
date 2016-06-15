025c4871aa9d3283a8f825fff480465429b4a134
Patrick Carroll <patrickcarroll@learnzillion.com>
2016-06-14 21:18:26 -0400

    Paginate these collections

--- app/controllers/moderators_controller.rb
+++ app/controllers/moderators_controller.rb
@@ -2,7 +2,7 @@ class ModeratorsController < ApplicationController
   before_filter :requires_moderator!
 
   def index
-    @mod_pending_trades = Trade.joins(:participants).merge(Participant.needing_moderator_approval)
-    @not_accepted_trades = Trade.joins(:participants).merge(Participant.old_and_not_yet_accepted)
+    @mod_pending_trades = Trade.joins(:participants).merge(Participant.needing_moderator_approval).page(params[:mod_pending_page])
+    @not_accepted_trades = Trade.joins(:participants).merge(Participant.old_and_not_yet_accepted).page(params[:not_accepted_page])
   end
 end
